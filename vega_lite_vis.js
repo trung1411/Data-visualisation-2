95adfcf5307497a32563dee7ac93f286fb8b43eb
John Tran <trung.14112002@gmail.com>
2023-10-04 13:20:50 +1100

    update to project data vis 2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,9 @@ var vg_1 = "barchart2.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+
+var vg_2 = "map.vg.json";
+vegaEmbed("#chloro_map", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
