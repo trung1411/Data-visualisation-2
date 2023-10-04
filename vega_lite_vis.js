287c3aab6c542f30ec09864b4213daf82e634d6f
John Tran <trung.14112002@gmail.com>
2023-10-04 14:09:39 +1100

    changes to html file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "barchart2.vg.json";
+var vg_1 = "fat_gender_bar.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
