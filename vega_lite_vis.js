4e59446da336d30834b32f099c740ba9adc1be98
John Tran <trung.14112002@gmail.com>
2023-10-04 23:10:20 +1100

    update graphs for data visualisation 2

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -9,7 +9,7 @@ vegaEmbed("#chloro_map", vg_2).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "line_char.vg.json";
+var vg_3 = "line_chart.vg.json";
 vegaEmbed("#line_chart", vg_3).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
