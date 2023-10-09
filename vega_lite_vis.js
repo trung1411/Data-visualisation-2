79b7ddeab75267f16394a49debd75025463dc339
John Tran <trung.14112002@gmail.com>
2023-10-09 16:09:09 +1100

    update javascript files

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -18,3 +18,9 @@ var vg_4 = "bar_chart_crash_type.vg.json";
 vegaEmbed("#bar_chart2", vg_4).then(function(result) {
         // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
         }).catch(console.error);
+
+
+var vg_5 = "lasagna_heat_map.vg.json";
+vegaEmbed("#heat_map", vg_5).then(function(result) {
+        // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.vie
+        }).catch(console.error);
