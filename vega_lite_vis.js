1e8fbd7347d04b5a00a38fcf5951d0e379ce3231
John Tran <trung.14112002@gmail.com>
2023-10-16 10:43:33 +1100

    update js file

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,26 +1,26 @@
-var vg_1 = "fat_gender_bar.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "crash_type_age_group.vg.json";
+vegaEmbed("#stacked_bar_chart", vg_1, {action: false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 var vg_2 = "map.vg.json";
-vegaEmbed("#chloro_map", vg_2).then(function(result) {
+vegaEmbed("#chloro_map",vg_2, {action: false}).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "line_chart.vg.json";
-vegaEmbed("#line_chart", vg_3).then(function(result) {
+vegaEmbed("#line_chart", vg_3, {action: false}).then(function(result) {
     // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
 var vg_4 = "bar_chart_crash_type.vg.json";
-vegaEmbed("#bar_chart2", vg_4).then(function(result) {
+vegaEmbed("#bar_chart2", vg_4, {action: false}).then(function(result) {
         // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
         }).catch(console.error);
 
 
 var vg_5 = "lasagna_heat_map.vg.json";
-vegaEmbed("#heat_map", vg_5).then(function(result) {
+vegaEmbed("#heat_map", vg_5, {action: false}).then(function(result) {
         // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.vie
         }).catch(console.error);
